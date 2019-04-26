8e6aac0e3eca3b834e34029a51d735d3725f4529
kevinsalles <kevinsalles@users.noreply.github.com>
2019-04-26 10:48:41 -0400

    Update DI.WebPush-sw.js

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/4164/12890/FGQFNnJcpbRMSvVfpVIDf7l955gx84d2/DI.WebPush-min.js');
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/4164/12890/FGQFNnJcpbRMSvVfpVIDf7l955gx84d2/DI.WebPush-sw-lib.js');
