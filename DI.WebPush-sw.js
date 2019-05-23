12bd9a1a05cfb22ac1cce12185df6e852c9e732f
ksalles <50966894+ksalles@users.noreply.github.com>
2019-05-23 15:49:53 -0400

    Update DI.WebPush-sw.js

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/4164/12890/FGQFNnJcpbRMSvVfpVIDf7l955gx84d2/DI.WebPush-sw-lib.js');
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/4164/13487/OA7hEkN1k1vSnuwAeSuKgRtUjorersDZ/DI.WebPush-sw-lib.js');
